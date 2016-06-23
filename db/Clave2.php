51861de59278c84f52081b19773f917323f5d904
Pablo Moya <pmmoyapablo@gmail.com>
2016-06-23 15:58:00 -0400

    V-3.0.0.4 (Production) Actualización de credenciales de conección a Bases de Datos en producción

--- db/Clave2.php
+++ db/Clave2.php
@@ -2,19 +2,19 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-
+/*
 $hostname_conexfamdd = "127.0.0.1";
 $database_conexfamdd = "mefamdd_prestashop";
 $username_conexfamdd = "root";
 $password_conexfamdd = "1234";
- 
+ */
  
 // Produccion
-/*
- $hostname_conexfamdd = "localhost";
-$database_conexfamdd = "wwwmicro_prestashop";
-$username_conexfamdd = "wwwmicro_admin";
-$password_conexfamdd = "590079oi";
- */
+
+$hostname_conexfamdd = "localhost";
+$database_conexfamdd = "mefamdd_prestashop";
+$username_conexfamdd = "mefamdd_root";
+$password_conexfamdd = "TAHV+5*TS?}H";
+ 
 //$conexfamdd = mysql_pconnect($hostname_conexfamdd, $username_conexfamdd, $password_conexfamdd) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
